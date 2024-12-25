3312d57f688839a153252029aa57260ab62de366
Terrencezifeng <89788335+Terrencezifeng@users.noreply.github.com>
2024-12-25 12:20:55 +0800

    Update and rename LB,py to LB.py
    轮询算法实现

--- LB.py
+++ LB.py
@@ -2,8 +2,7 @@ from ryu.base import app_manager
 from ryu.controller import ofp_event
 from ryu.controller.handler import MAIN_DISPATCHER, CONFIG_DISPATCHER, set_ev_cls
 from ryu.ofproto import ofproto_v1_3
-from ryu.lib.packet import packet
-from ryu.lib.packet import ethernet
+from ryu.lib.packet import packet, ethernet, arp, icmp
 from ryu.lib.packet import ether_types
 
 class RoundRobinLB(app_manager.RyuApp):
@@ -11,9 +10,7 @@ class RoundRobinLB(app_manager.RyuApp):
 
     def __init__(self, *args, **kwargs):
         super(RoundRobinLB, self).__init__(*args, **kwargs)
-        # 定义后端服务器的IP地址列表
         self.servers = ['10.0.0.2', '10.0.0.3', '10.0.0.4']
-        # 用于跟踪当前选择的服务器索引，初始化为0
         self.current_server = 0
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
@@ -21,21 +18,14 @@ class RoundRobinLB(app_manager.RyuApp):
         datapath = ev.msg.datapath
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
-
-        # 安装一个table-miss流表项，将未匹配的数据包发送到控制器处理
         match = parser.OFPMatch()
-        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
-                                          ofproto.OFPCML_NO_BUFFER)]
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER, ofproto.OFPCML_NO_BUFFER)]
         self.add_flow(datapath, 0, match, actions)
 
     def add_flow(self, datapath, priority, match, actions, buffer_id=None):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
-
-        # 定义指令，这里是应用指定的动作
-        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
-                                             actions)]
-        # 如果有buffer_id，则包括在FlowMod消息中，否则直接忽略它
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
         if buffer_id:
             mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,
                                     priority=priority, match=match,
@@ -56,35 +46,38 @@ class RoundRobinLB(app_manager.RyuApp):
         pkt = packet.Packet(msg.data)
         eth = pkt.get_protocols(ethernet.ethernet)[0]
 
-        # 忽略LLDP包，它们用于交换拓扑信息
         if eth.ethertype == ether_types.ETH_TYPE_LLDP:
             return
 
         dst = eth.dst
         src = eth.src
+        self.logger.info("packet in %s %s %s %s", datapath.id, src, dst, in_port)
 
-        dpid = format(datapath.id, "d").zfill(16)
-        self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
+        # 添加对 ICMP 和 ARP 数据包的处理
+        if eth.ethertype in [ether_types.ETH_TYPE_ARP, ether_types.ETH_TYPE_IP]:
+            if eth.ethertype == ether_types.ETH_TYPE_IP:
+                ip_pkt = pkt.get_protocols(ipv4.ipv4)[0]
+                if isinstance(pkt.get_protocols(icmp.icmp)[0], icmp.icmp):
+                    # 为 ICMP 数据包选定后端主机
+                    self.current_server = (self.current_server + 1) % len(self.servers)
+                    selected_server = self.servers[self.current_server]
+                    match = parser.OFPMatch(in_port=in_port)
+                    actions = [parser.OFPActionSetField(ipv4_dst=selected_server),
+                               parser.OFPActionOutput(ofproto.OFPP_NORMAL)]
+            else:
+                # 处理 ARP 数据包
+                self.logger.info("Processing ARP packet")
+                match = parser.OFPMatch(in_port=in_port)
+                actions = [parser.OFPActionOutput(ofproto.OFPP_FLOOD)]  # Flood ARP requests
 
-        # 使用轮询算法选择下一个后端服务器
-        self.current_server = (self.current_server + 1) % len(self.servers)
-        selected_server = self.servers[self.current_server]
-        
-        # 创建流表匹配规则，并定义动作集（修改目的IP并输出）
-        match = parser.OFPMatch(in_port=in_port)
-        actions = [parser.OFPActionSetField(ipv4_dst=selected_server),
-                   parser.OFPActionOutput(ofproto.OFPP_NORMAL)]
+            # 添加流表项
+            self.add_flow(datapath, 1, match, actions, msg.buffer_id)
 
-        # 添加新的流表项，把数据包重定向到选定的服务器
+        # 处理到其他数据包的默认路径
         if msg.buffer_id != ofproto.OFP_NO_BUFFER:
             self.add_flow(datapath, 1, match, actions, msg.buffer_id)
-            return
         else:
-            data = None
-            if msg.buffer_id == ofproto.OFP_NO_BUFFER:
-                data = msg.data
-
             out = parser.OFPPacketOut(
                 datapath=datapath, buffer_id=msg.buffer_id,
-                in_port=in_port, actions=actions, data=data)
+                in_port=in_port, actions=actions, data=msg.data)
             datapath.send_msg(out)
